19f77a6797afbfb7140b531266d5b5b991e23934
u8slvn <u8slvn@gmail.com>
2024-10-28 20:52:02 +0100

    feat: add cli base

---
+++ src/commands/cli.rs
@@ -0,0 +1,22 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, bin_name = "otp76", disable_help_subcommand = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Exec(super::exec::Cli),
+}
+
+impl Cli {
+    pub fn exec(&self) -> Result<()> {
+        match &self.command {
+            Commands::Exec(cli) => cli.exec(),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod commands;
+
+use anyhow::Result;
+use clap::Parser;
+
+use crate::commands::cli::Cli;
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    cli.exec()
+}
